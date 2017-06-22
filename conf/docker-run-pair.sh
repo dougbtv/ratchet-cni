6b415a8d5c8633434cbba73ee03cda27c370471e
dougbtv <dosmith@redhat.com>
2017-06-22 15:09:11 -0400

    [ansible][conf] updates to ansible and configs

--- conf/docker-run-pair.sh
+++ conf/docker-run-pair.sh
@@ -5,17 +5,18 @@
 
 # Example usage: ./docker-run.sh --rm busybox /sbin/ifconfig
 
-contid=$(docker run -d --net=none --label-file ./primary.label busybox:latest /bin/sleep 10000000)
+contid=$(docker run --name pair_infra -d --net=none --label-file ./pair.label busybox:latest /bin/sleep 10000000)
 pid=$(docker inspect -f '{{ .State.Pid }}' $contid)
 netnspath=/proc/$pid/ns/net
 
 ./exec-plugins.sh add $contid $netnspath
 
-function cleanup() {
-  ./exec-plugins.sh del $contid $netnspath
-  docker kill $contid >/dev/null
-}
-trap cleanup EXIT
+# Not cleaning up for now... Doug.
+# function cleanup() {
+#   ./exec-plugins.sh del $contid $netnspath
+#   docker kill $contid >/dev/null
+# }
+# trap cleanup EXIT
 
 docker run --net=container:$contid $@
 
---
+++ conf/docker-run-primary.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Run a docker container with network namespace set up by the
+# CNI plugins.
+
+# Example usage: ./docker-run.sh --rm busybox /sbin/ifconfig
+
+contid=$(docker run --name primary_infra -d --net=none --label-file ./primary.label busybox:latest /bin/sleep 10000000)
+pid=$(docker inspect -f '{{ .State.Pid }}' $contid)
+netnspath=/proc/$pid/ns/net
+
+./exec-plugins.sh add $contid $netnspath
+
+# Not cleaning up for now... Doug.
+# function cleanup() {
+#   ./exec-plugins.sh del $contid $netnspath
+#   docker kill $contid >/dev/null
+# }
+# trap cleanup EXIT
+
+docker run --net=container:$contid $@
+
