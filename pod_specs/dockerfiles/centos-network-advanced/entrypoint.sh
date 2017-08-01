db37f4e4df2107d574f32b17eb31f42d9bc0a88d
dougbtv <dosmith@redhat.com>
2017-08-01 08:15:41 -0400

    [bugfix][dockerfiles] needed a wait in the entrypoint for the centos endpoints image

--- pod_specs/dockerfiles/centos-network-advanced/entrypoint.sh
+++ pod_specs/dockerfiles/centos-network-advanced/entrypoint.sh
@@ -5,6 +5,15 @@ if [ -z ${DEFAULT_GW+x} ]; then echo "DEFAULT_GW env var is unset, exiting"; exi
 if [ -z ${DEFAULT_DEV+x} ]; then echo "DEFAULT_DEV env var is unset, exiting"; exit 1; fi
 if [ -z ${WAN_IP+x} ]; then echo "WAN_IP env var is unset, exiting"; exit 1; fi
 
+# wait until you see target device
+tries=0
+until ip a | grep $DEFAULT_DEV
+do
+  sleep 1
+  let "tries+=1"
+  if [ $tries -ge 30 ]; then echo "couldn't find $DEFAULT_DEV"; exit 1; fi
+done
+
 echo "Setting default route to: $DEFAULT_GW via $DEFAULT_DEV"
 ip route add default via $DEFAULT_GW dev $DEFAULT_DEV
 
@@ -13,4 +22,3 @@ ip a add $WAN_IP/255.255.255.255 dev lo;
 
 # Sleep forever.
 while :; do sleep 10; done
-
